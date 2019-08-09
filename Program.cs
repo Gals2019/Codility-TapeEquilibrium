2eed0573e82b95427be3432ce4623d9ed5001c3c
gals <developergals@gmail.com>
2019-08-09 19:36:00 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility_TapeEquilibrium
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            int[] A = {3,1,2,4,3};
+
+            List<long> diffs = new List<long>();
+            long left = 0;
+            long sum = A.Sum();
+            foreach (int item in A)
+            {
+                left += item;
+                long calculo = (sum - left) - left;
+                diffs.Add(Math.Abs(calculo));
+            }
+            return (int)diffs.Min();
+
+        }
+    }
+}
